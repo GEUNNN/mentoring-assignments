638aefffe024e9d3342b4640a6f92ebca01c5537
GEUNNN <geunhwa.lee.01@gmail.com>
2025-10-07 15:11:51 +0900

    add: contents (디테일) 페이지 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Search from "./search/Search";
+import Contents from "./contents/Contents";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 const rootElement = document.getElementById("root")!;
@@ -12,6 +13,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/search" element={<Search />} />
+      <Route path="/contents/:id" element={<Contents />} />
     </Routes>
   </BrowserRouter>
 );
