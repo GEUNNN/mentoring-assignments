a824478010dfce8cbd6b6355abe0021f695bee55
GEUNNN <geunhwa.lee.01@gmail.com>
2025-11-07 11:25:55 +0900

    fix: miniCarousel 컴포넌트에 suspense 추가

--- src/main/components/MiniCarouselSection.tsx
+++ src/main/components/MiniCarouselSection.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, Suspense, useEffect, useRef, useState } from "react";
 import { IMG_BASE_URL } from "../../apis/config";
 import { Link } from "react-router";
 
@@ -41,58 +41,60 @@ const MiniCarouselSection: FC<MiniCarouselSectionProps> = ({ list, label }) => {
     transition: isTransitioning.current ? "transform 0.3s ease-in-out" : "none",
   };
 
-  if (!list || list.length === 0) {
-    return <section className="mini-carousel-container">Loading...</section>;
-  }
+  const fallbackContent = (
+    <section className="mini-carousel-container">Loading...</section>
+  );
 
   return (
-    <div className="mini-carousel-wrapper">
-      <section className="mini-carousel-container">
-        <h2 className="mini-carousel-label">{label}</h2>
-        <div
-          className="mini-slide-container"
-          ref={carouselRef}
-          style={slideTransformStyle}
-        >
-          {extenedList.map((item: any, idx: number) => (
-            <div
-              key={item.id ? `movie-${item.id}` : `clone-${idx}`}
-              className="mini-slide-item"
-            >
-              <Link
-                to={`/contents/${item.id}`}
-                state={{ type: "tv" }}
-                className="mini-carousel-card"
+    <Suspense fallback={fallbackContent}>
+      <div className="mini-carousel-wrapper">
+        <section className="mini-carousel-container">
+          <h2 className="mini-carousel-label">{label}</h2>
+          <div
+            className="mini-slide-container"
+            ref={carouselRef}
+            style={slideTransformStyle}
+          >
+            {extenedList.map((item: any, idx: number) => (
+              <div
+                key={item.id ? `movie-${item.id}` : `clone-${idx}`}
+                className="mini-slide-item"
               >
-                <div className="mini-poster-wrapper">
-                  <img
-                    src={`${IMG_BASE_URL}${item.poster_path}`}
-                    alt={item.title}
-                    loading="lazy"
-                    className="mini-carousel-image"
-                  />
-                  <div className="card-overlay">
-                    <h3
-                      className="card-logo"
-                      style={{ fontSize: "20px", fontWeight: "bold" }}
-                    >
-                      {item.title}
-                    </h3>
-                    <p className="card-description">{item.overview}</p>
+                <Link
+                  to={`/contents/${item.id}`}
+                  state={{ type: "tv" }}
+                  className="mini-carousel-card"
+                >
+                  <div className="mini-poster-wrapper">
+                    <img
+                      src={`${IMG_BASE_URL}${item.poster_path}`}
+                      alt={item.title}
+                      loading="lazy"
+                      className="mini-carousel-image"
+                    />
+                    <div className="card-overlay">
+                      <h3
+                        className="card-logo"
+                        style={{ fontSize: "20px", fontWeight: "bold" }}
+                      >
+                        {item.title}
+                      </h3>
+                      <p className="card-description">{item.overview}</p>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-        <button onClick={() => moveCarousel(-1)} className="nav-button left">
-          <span>❮</span>
-        </button>
-        <button onClick={() => moveCarousel(1)} className="nav-button right">
-          <span>❯</span>
-        </button>
-      </section>
-    </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+          <button onClick={() => moveCarousel(-1)} className="nav-button left">
+            <span>❮</span>
+          </button>
+          <button onClick={() => moveCarousel(1)} className="nav-button right">
+            <span>❯</span>
+          </button>
+        </section>
+      </div>
+    </Suspense>
   );
 };
 
