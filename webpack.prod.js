23a56683a1cfcbc12ae96790d4048c102cc31f5c
GEUNNN <geunhwa.lee.01@gmail.com>
2025-11-20 12:16:18 +0900

    fix: contenthash 수정

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -19,5 +20,10 @@ module.exports = merge(common, {
       async: false,
     }),
     new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // 템플릿 파일 경로
+      filename: "index.[contenthash].html", // 생성될 HTML 파일 이름
+      inject: "body", // 스크립트를 body 태그 끝에 삽입
+    }),
   ],
 });
