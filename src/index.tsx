adf7b4cde48c718b1870bda938f3d6db4e5580c9
GEUNNN <geunhwa.lee.01@gmail.com>
2025-11-20 12:21:02 +0900

    fix: error-boundary 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,37 @@ import Main from "./Main/Main";
 import Search from "./search/Search";
 import Contents from "./contents/Contents";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQueryErrorResetBoundary,
+} from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+
+function App() {
+  const { reset } = useQueryErrorResetBoundary();
+
+  const Fallback = ({ resetErrorBoundary }) => (
+    <div>
+      <p>앗! 페이지를 다시 로딩해주세요. (에러 바운더리)</p>
+      <button onClick={() => resetErrorBoundary()}>
+        다시 시도 (Try again)
+      </button>
+    </div>
+  );
+
+  return (
+    <ErrorBoundary onReset={reset} FallbackComponent={Fallback}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/contents/:id" element={<Contents />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
+  );
+}
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
@@ -13,12 +43,6 @@ const queryClient = new QueryClient();
 
 root.render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/contents/:id" element={<Contents />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </QueryClientProvider>
 );
