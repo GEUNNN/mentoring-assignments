a89d2873fccd9683509c7c222d7e10dc8ab3901c
GEUNNN <geunhwa.lee.01@gmail.com>
2025-11-26 15:59:15 +0900

    fix: 루트 디렉토리 내 기존 파일 제거

--- src/query/credits.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCredits } from "../apis/axiosClient";
-import { queryKey } from "./query-key";
-
-export const useQueryGetCredits = (id: string) =>
-  useQuery({
-    queryKey: queryKey.credits(id),
-    queryFn: () => getCredits(id),
-  });
--- src/query/review.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getReviewList } from "../apis/axiosClient";
-import { queryKey } from "./query-key";
-
-export const useQueryGetReviewList = (id: string) =>
-  useQuery({
-    queryKey: queryKey.review(id),
-    queryFn: () => getReviewList(id),
-  });
