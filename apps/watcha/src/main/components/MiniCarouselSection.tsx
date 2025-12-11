68a6b1733719741f545ca812589e5b2d08cb6920
GEUNNN <geunhwa.lee.01@gmail.com>
2025-12-11 21:25:50 +0900

    fix: miniCarousel section 내비게이션시 type 수정

--- apps/watcha/src/main/components/MiniCarouselSection.tsx
+++ apps/watcha/src/main/components/MiniCarouselSection.tsx
@@ -41,7 +41,12 @@ const MiniCarouselSection: FC<MiniCarouselSectionProps> = ({
             {extenedList.map((item: MainItem) => (
               <div key={item.id} className="mini-slide-item">
                 <div
-                  onClick={() => handleClickItem(item.id, "tv")}
+                  onClick={() =>
+                    handleClickItem(
+                      item.id,
+                      item.video === false ? "movie" : "tv"
+                    )
+                  }
                   className="mini-carousel-card"
                 >
                   <div className="mini-poster-wrapper">
